60b758e06326ce4cfdf286b6dfc7b9895122a6be
briane10 <brianestes14@gmail.com>
2025-05-31 16:23:45 -0700

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1 style={{ color: '#FFAF00' }}>Fuck Trey</h1>
+      <p>
+        Coolest of red bears with a slight sun allergy, Trey boi is a money brah with a mad ass golf swing and a sweet lead guitar. Shaggy and swaggy with man muscles, (who can’t beat his old man arm wrestling yet), off to university he goes. Don’t forget to set an alarm though my dude so you don’t miss class in the early morning; we all know that’s not your thang! Now, go slay it. Kid stuff is over my boy.
+      </p>
+    </div>
+  );
+}
+
+export default App;
